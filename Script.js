96fd8d05a291deb6c3495af799dc4350a6a4343a
Saniya khan <133801644+Saniyakhan7543@users.noreply.github.com>
2023-12-26 17:39:07 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,11 @@
+
+
+    function Add(){
+        var a,b,c;
+        a=Number(document.getElementById("first").value);
+        b=Number(document.getElementById("second").value);
+        c=a+b;
+        document.getElementById("answer").value= c;
+        }
+
+       
